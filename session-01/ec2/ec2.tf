9c616b5295fe32ddb029c2c56da665797377e9dd
uddishd <chaitu.chaitanya87@gmail.com>
2024-01-03 22:50:55 +0530

    terraform variables

---
+++ session-01/ec2/ec2.tf
@@ -0,0 +1,37 @@
+resource "aws_instance" "web" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.roboshop-all.id]
+
+  tags = {
+    Name = "Terraform-inst"
+  }
+}
+
+resource "aws_security_group" "roboshop-all" {
+  name        = var.sg-name
+  description = var.sg-description
+  #vpc_id      = aws_vpc.main.id
+
+
+ingress {
+    description      = "Allow all ports"
+    from_port        = var.inbound-from-port
+    to_port          = 0
+    protocol         = "tcp"
+    cidr_blocks      = var.cidr_blocks
+    #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    #ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "roboshop-all-aws"
+  }
+}
